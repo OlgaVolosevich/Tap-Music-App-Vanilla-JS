475c023903ab092b1cc48f4da6bf0a1d28534fcd
Olga Volosevich <olga.lucky.v@gmail.com>
2020-10-05 19:27:18 +0300

    feat: add dynamic music keys

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("Hello!")
+window.onload = function () {
+  const keys = Array.from(document.getElementsByClassName("keys__item"));
+  keys.forEach((key) => {
+    const audio = Array.from(key.children).filter((child) =>
+      child.classList.contains("audio")
+    )[0];
+    key.addEventListener("click", () => {
+      audio.currentTime = 0;
+      audio.play();
+    });
+  });
+};
